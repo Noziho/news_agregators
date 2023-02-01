fc80f4f7dcf08a6f419f2248f3726d0cc81380de
Noziho <noah.decroix3@gmail.com>
2023-02-01 19:28:18 +0100

    Split one page to 3 for each category + show one article on click

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends AbstractController
     #[Route('/bitcoin', name: 'app_home_test')]
     public function bitcoin(NewsAgregatorService $agregatorService): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('article/bitcoin.html.twig', [
             'articles' => $agregatorService->getBitcoinArticles(),
         ]);
     }
@@ -22,7 +22,7 @@ class ArticleController extends AbstractController
     #[Route('/apple', name: 'app_home_apple')]
     public function apple(NewsAgregatorService $agregatorService)
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('article/apple.html.twig', [
             'articles' => $agregatorService->getAppleArticles(),
         ]);
     }
@@ -30,11 +30,36 @@ class ArticleController extends AbstractController
     /**
      * @throws NewsApiException
      */
-    #[Route('/currentArticle/{id}')]
+    #[Route('/currentGeneralArticle/{id}')]
     public function showCurrentGeneralArticle(NewsAgregatorService $agregatorService, int $id): Response
     {
         return $this->render('article/showArticle.html.twig', [
             'articles' => $agregatorService->getGeneralArticles(),
+            'id' => $id,
+        ]);
+    }
+
+    /**
+     * @throws NewsApiException
+     */
+    #[Route('/currentBitcoinArticle/{id}')]
+    public function showCurrentBitcoinArticle(NewsAgregatorService $agregatorService, int $id): Response
+    {
+        return $this->render('article/showArticle.html.twig', [
+            'articles' => $agregatorService->getBitcoinArticles(),
+            'id' => $id,
+        ]);
+    }
+
+
+    /**
+     * @throws NewsApiException
+     */
+    #[Route('/currentAppleArticle/{id}')]
+    public function showCurrentAppleArticle(NewsAgregatorService $agregatorService, int $id): Response
+    {
+        return $this->render('article/showArticle.html.twig', [
+            'articles' => $agregatorService->getAppleArticles(),
             'id' => $id,
         ]);
     }
